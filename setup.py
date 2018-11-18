e11c876e222ecc5a8fff3ae529d0a83d3000e1d7
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-11-18 15:17:33 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='python_athena_tools',
+      version='0.0.1',
+      description='A wrapper around Athena to load query results into Python memory as fast as possible',
+      url='https://github.com/moj-analytical-services/python_athena_tools',
+      author='Robin Linacre, Karik Isichei',
+      author_email='robinlinacre@hotmail.com',
+      license='MIT',
+      setup_requires=['pandas, pyarrow, s3fs, boto3'],
+      test_requires=["pylint", "coverage", "codecov"],
+      zip_safe=False)
